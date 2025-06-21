6dabc54c71151ae541ec758a73d94913f3dcc239
manisht07 <83353539+manisht07@users.noreply.github.com>
2025-06-21 14:30:58 +0530

    Add Glue job control lambdas and routes

---
+++ api/job/terminate/main.py
@@ -0,0 +1,77 @@
+"""
+Copyright 2023 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+
+Licensed under the Amazon Software License (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+  http://aws.amazon.com/asl/
+
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+"""
+
+import boto3
+import json
+import logging
+import os
+import traceback
+
+# region Logging
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+logger = logging.getLogger()
+if logger.hasHandlers():
+    logger.setLevel(LOG_LEVEL)
+else:
+    logging.basicConfig(level=LOG_LEVEL)
+# endregion
+
+glue = boto3.client("glue")
+
+def mask_sensitive_data(event):
+    # remove sensitive data from request object before logging
+    keys_to_redact = ["authorization"]
+    result = {}
+    for k, v in event.items():
+        if isinstance(v, dict):
+            result[k] = mask_sensitive_data(v)
+        elif k in keys_to_redact:
+            result[k] = "<redacted>"
+        else:
+            result[k] = v
+    return result
+
+
+def build_response(http_code, body):
+    return {
+        "headers": {
+            "Cache-Control": "no-cache, no-store",
+            "Content-Type": "application/json",
+        },
+        "statusCode": http_code,
+        "body": body,
+    }
+
+
+def lambda_handler(event, context):
+    logger.info(mask_sensitive_data(event))
+
+    try:
+        body = json.loads(event["body"]) if "body" in event else json.loads(event)
+        job_name = body["job_name"]
+
+        glue.delete_job(JobName=job_name)
+
+        return build_response(200, json.dumps({"message": "Job deleted"}))
+
+    except Exception:
+        logger.error(traceback.format_exc())
+        return build_response(500, "Server Error")
+
+
+if __name__ == "__main__":
+    example_event = {}
+    response = lambda_handler(example_event, {})
+    print(json.dumps(response))
